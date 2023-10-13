09ac36cc54a4f6fb3d7ffa971f6bfa426eb496f2
realdevvora <legitvoradev@gmail.com>
2023-10-12 21:44:31 -0400

    Updated Homepage CSS

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -14,9 +14,9 @@ function Home() {
         <div className="home--review--example">
         <h3>View ratings on courses you plan to take!</h3>
           <div className="home--review--courses">
-            <MiniCourse name={"ANT102"} description={"Anthropology is the global and holistic study of human biology and behaviour, and includes four subfields: biological anthropology, archaeology, sociocultural anthropology and linguistic anthropology. The material covered is directed to answering the question..."}/>
-            <MiniCourse name={"CSC148"} description={"The scientific method and the modern theory of evolution as an introduction to biology. The principles of evolution, transmission and evolutionary genetics are developed in lectures and laboratories."}/>
-            <MiniCourse name={"BIO152"} description={"Abstract data types and data structures for implementing them. Linked data structures. Encapsulation and information-hiding. Object-oriented programming. Specifications. Analyzing the efficiency of programs. Recursion. This course assumes programming..."}/>
+            <MiniCourse name={"ANT102H5 • Introduction to Sociocultural and Linguistic Anthropology"} description={"Anthropology is the global and holistic study of human biology and behaviour, and includes four subfields: biological anthropology, archaeology, sociocultural anthropology and linguistic anthropology. The material covered is directed to answering the question..."} link={"/ANT102H5%20•%20Introduction%20to%20Sociocultural%20and%20Linguistic%20Anthropology"}/>
+            <MiniCourse name={"BIO152H5 • Introduction to Evolution and Evolutionary Genetics"} description={"The scientific method and the modern theory of evolution as an introduction to biology. The principles of evolution, transmission and evolutionary genetics are developed in lectures and laboratories."} link={"http://localhost:3000/BIO152H5%20%E2%80%A2%20Introduction%20to%20Evolution%20and%20Evolutionary%20Genetics"}/>
+            <MiniCourse name={"CSC148H5 • Introduction to Computer Science"} description={"Abstract data types and data structures for implementing them. Linked data structures. Encapsulation and information-hiding. Object-oriented programming. Specifications. Analyzing the efficiency of programs. Recursion. This course assumes programming..."} link={"/CSC148H5%20•%20Introduction%20to%20Computer%20Science"}/>
           </div>
         </div>
         <div className="home--review--anecdote">
--- front-end/src/components/homepage/MiniCourse.jsx
+++ front-end/src/components/homepage/MiniCourse.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 
 export default function MiniCourse(props) {
-    const {name, description} = props
+    const {name, description, link} = props
+    const navigate = useNavigate()
 
 
     return (
-        <div className="home--page--course">
-            <h1>{name}</h1>
-            <div className="home--review--scores">
-                <h4>{description}</h4>
+            <div className="home--page--course" onClick={() => navigate(link)}>
+                <h1>{name}</h1>
+                <div className="home--review--scores">
+                    <h4>{description}</h4>
+                </div>
             </div>
-        </div>
     )
 }
