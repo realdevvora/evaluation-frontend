0ed7717f5e863c076aaaa337ba65730036986a22
realdevvora <legitvoradev@gmail.com>
2024-08-08 22:29:17 -0400

    First smoke test

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -8,7 +8,7 @@ import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 
 // for production
-disableReactDevTools()
+// disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
