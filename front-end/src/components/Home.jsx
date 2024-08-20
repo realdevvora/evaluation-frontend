ed9658bab918d88f24ef557b16b7e94e8e25767b
realdevvora <legitvoradev@gmail.com>
2024-08-19 21:06:48 -0400

    UI Updates

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -9,10 +9,10 @@ function Home() {
   return (
     <div className="home--page">
       <div className="home--hero">
-        <h3>honest opinions of your fellow peers.</h3>
+        <h3>Get the inside scoop on the courses you’re eyeing</h3>
       </div>
         <div className="home--review--example">
-        <h3>View ratings on courses you plan to take!</h3>
+        <h3>View ratings for courses you plan to take:</h3>
           <div className="home--review--courses">
             <MiniCourse name={"ANT102H5 • Introduction to Sociocultural and Linguistic Anthropology"} description={"Anthropology is the global and holistic study of human biology and behaviour, and includes four subfields: biological anthropology, archaeology, sociocultural anthropology and linguistic anthropology. The material covered is directed to answering the question..."} link={"/ANT102H5%20•%20Introduction%20to%20Sociocultural%20and%20Linguistic%20Anthropology"}/>
             <MiniCourse name={"BIO152H5 • Introduction to Evolution and Evolutionary Genetics"} description={"The scientific method and the modern theory of evolution as an introduction to biology. The principles of evolution, transmission and evolutionary genetics are developed in lectures and laboratories."} link={"/BIO152H5%20%E2%80%A2%20Introduction%20to%20Evolution%20and%20Evolutionary%20Genetics"}/>
@@ -20,10 +20,12 @@ function Home() {
           </div>
         </div>
         <div className="home--review--anecdote">
-          <h3>Post ratings that encourage students to take courses, or discourage them from wasting their time and money.</h3>
+          <h3>Take courses that enable you to explore niche fields 🔎, growing industries 📈, or stimulate your passion for learning 🧠</h3>
           <img src={review_anecdotes} alt="" />
         </div>
-      <Footer />
+        <div className="home--review--promotion">
+          {/* <h3>Encourage others to take courses that will help them grow their academic and professional journey 🤝</h3> */}
+        </div>
     </div>
   )
 }
